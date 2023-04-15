c6535d3d848221cb0eb11a81db5c7a6466430ef1
Usama <usamatahirsatti@gmail.com>
2023-04-16 01:31:21 +0500

    design select language widget

--- lib/view/settings/setting_screen.dart
+++ lib/view/settings/setting_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:one_click_time_sheet/utills/constants/colors.dart';
+import 'package:one_click_time_sheet/utills/constants/text_styles.dart';
 
 class SettingScreen extends StatefulWidget {
   const SettingScreen({Key? key}) : super(key: key);
@@ -8,8 +11,98 @@ class SettingScreen extends StatefulWidget {
 }
 
 class _SettingScreenState extends State<SettingScreen> {
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Settings',
+          style: CustomTextStyle.kHeading2,
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10.w),
+        child: Column(
+          children: [
+            SizedBox(height: 10.h),
+            const ChooseLanguageWidget(),
+          ],
+        ),
+      ),
+    );
   }
 }
+
+class ChooseLanguageWidget extends StatefulWidget {
+  const ChooseLanguageWidget({Key? key}) : super(key: key);
+
+  @override
+  State<ChooseLanguageWidget> createState() => _ChooseLanguageWidgetState();
+}
+
+class _ChooseLanguageWidgetState extends State<ChooseLanguageWidget> {
+  List languagesList = ['English', 'Egyption'];
+  String selectedLanguage = 'English';
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'Language',
+          style: CustomTextStyle.kHeading2,
+        ),
+        Container(
+          width: 90.w,
+          height: 30.h,
+          padding: EdgeInsets.only(
+            left: 4.w,
+            right: 4.w,
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5.r),
+            border: Border.all(
+              color: blackColor,
+              width: .5.w,
+            ),
+          ),
+          child: DropdownButtonHideUnderline(
+            child: DropdownButton(
+              dropdownColor: whiteColor,
+              icon: Icon(
+                Icons.keyboard_arrow_down,
+                size: 20.h,
+                color: blackColor,
+              ),
+              isExpanded: true,
+              hint: Text(
+                'Select',
+                style: CustomTextStyle.kBodyText2,
+              ),
+              value: selectedLanguage,
+              items: [
+                for (int i = 0; i < languagesList.length; i++)
+                  DropdownMenuItem(
+                    value: languagesList[i],
+                    child: Text(
+                      languagesList[i],
+                      style: CustomTextStyle.kBodyText2,
+                    ),
+                  ),
+              ],
+              onChanged: (val) {
+                setState(() {
+                  selectedLanguage = val.toString();
+                });
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
