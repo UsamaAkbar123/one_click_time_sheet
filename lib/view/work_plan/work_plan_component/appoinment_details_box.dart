2c1fb6421831f37c417700e8e488a528b82a76bb
Usama <usamatahirsatti@gmail.com>
2023-07-15 22:35:47 +0500

    when user click on old workplan the edit and delete icon are not showing

--- lib/view/work_plan/work_plan_component/appoinment_details_box.dart
+++ lib/view/work_plan/work_plan_component/appoinment_details_box.dart
@@ -8,61 +8,83 @@ import 'package:one_click_time_sheet/view/work_plan/work_plan_component/alert_bo
 import 'package:one_click_time_sheet/view/work_plan/work_plan_component/appointment_text_widget.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
-class AppointmentDetailsBox extends StatelessWidget {
+class AppointmentDetailsBox extends StatefulWidget {
   final CalendarTapDetails? calendarTapDetail;
 
   const AppointmentDetailsBox({Key? key, this.calendarTapDetail})
       : super(key: key);
 
-  static PreferenceManager preferenceManager = PreferenceManager();
-  static Box box = Hive.box('workPlan');
+  @override
+  State<AppointmentDetailsBox> createState() => _AppointmentDetailsBoxState();
+}
+
+class _AppointmentDetailsBoxState extends State<AppointmentDetailsBox> {
+  PreferenceManager preferenceManager = PreferenceManager();
+  Box box = Hive.box('workPlan');
+  late Appointment tappedMeeting;
+  String meetingStatus = '';
+
+  @override
+  void initState() {
+    tappedMeeting =
+        widget.calendarTapDetail?.appointments?.first as Appointment;
+    final todayDate = DateTime(
+      DateTime.now().year,
+      DateTime.now().month,
+      DateTime.now().day,
+    );
+    if (tappedMeeting.startTime.isBefore(todayDate)) {
+      meetingStatus = 'old';
+    } else {
+      meetingStatus = 'new';
+    }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       iconPadding: EdgeInsets.only(top: 10.h, right: 10.w, bottom: 10.h),
-      icon: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          GestureDetector(
-            onTap: () {
-              final tappedMeeting =
-                  calendarTapDetail?.appointments?.first as Appointment;
-              box.delete(tappedMeeting.id).then((value) {
-                Navigator.of(context).pop();
-              });
-            },
-            child: Icon(
-              Icons.delete,
-              color: blackColor.withOpacity(0.4),
+      icon: meetingStatus == 'old'
+          ? const SizedBox()
+          : Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    box.delete(tappedMeeting.id).then((value) {
+                      Navigator.of(context).pop();
+                    });
+                  },
+                  child: Icon(
+                    Icons.delete,
+                    color: blackColor.withOpacity(0.4),
+                  ),
+                ),
+                SizedBox(width: 6.w),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AddWorkPlanBox(
+                          isEditMode: true,
+                          id: tappedMeeting.id.toString(),
+                          workPlanName: tappedMeeting.subject,
+                          startTime: tappedMeeting.startTime,
+                          endTime: tappedMeeting.endTime,
+                        );
+                      },
+                    );
+                  },
+                  child: Icon(
+                    Icons.edit,
+                    color: blackColor.withOpacity(0.4),
+                  ),
+                ),
+              ],
             ),
-          ),
-          SizedBox(width: 6.w),
-          GestureDetector(
-            onTap: () {
-              Navigator.of(context).pop();
-              showDialog(
-                context: context,
-                builder: (context) {
-                  final tappedMeeting =
-                      calendarTapDetail?.appointments?.first as Appointment;
-                  return AddWorkPlanBox(
-                    isEditMode: true,
-                    id: tappedMeeting.id.toString(),
-                    workPlanName: tappedMeeting.subject,
-                    startTime: tappedMeeting.startTime,
-                    endTime: tappedMeeting.endTime,
-                  );
-                },
-              );
-            },
-            child: Icon(
-              Icons.edit,
-              color: blackColor.withOpacity(0.4),
-            ),
-          ),
-        ],
-      ),
       title: const Align(
         alignment: Alignment.centerLeft,
         child: Text('Appointment Details'),
@@ -73,25 +95,25 @@ class AppointmentDetailsBox extends StatelessWidget {
         children: [
           AppointmentTextWidget(
             title: 'Subject: ',
-            value: calendarTapDetail?.appointments?.first.subject,
+            value: widget.calendarTapDetail?.appointments?.first.subject,
             valueTextColor: blueColor,
           ),
           AppointmentTextWidget(
             title: 'Start Time: ',
             value: preferenceManager.getTimeFormat == '12h'
-                ? DateFormat.jm()
-                    .format(calendarTapDetail?.appointments?.first.startTime)
-                : DateFormat.Hm()
-                    .format(calendarTapDetail?.appointments?.first.startTime),
+                ? DateFormat.jm().format(
+                    widget.calendarTapDetail?.appointments?.first.startTime)
+                : DateFormat.Hm().format(
+                    widget.calendarTapDetail?.appointments?.first.startTime),
             valueTextColor: greenColor,
           ),
           AppointmentTextWidget(
             title: 'End Time: ',
             value: preferenceManager.getTimeFormat == '12h'
-                ? DateFormat.jm()
-                    .format(calendarTapDetail?.appointments?.first.endTime)
-                : DateFormat.Hm()
-                    .format(calendarTapDetail?.appointments?.first.endTime),
+                ? DateFormat.jm().format(
+                    widget.calendarTapDetail?.appointments?.first.endTime)
+                : DateFormat.Hm().format(
+                    widget.calendarTapDetail?.appointments?.first.endTime),
             valueTextColor: redColor,
           ),
         ],
